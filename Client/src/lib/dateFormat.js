b836be319e3c3488e9d28e2d70b39c720a0f288d
ritik-sa0201 <ritik.sainicoding@gmail.com>
2025-07-05 14:03:15 +0530

    Delete Client directory

--- Client/src/lib/dateFormat.js
+++
@@ -1,9 +0,0 @@
-export const dateFormat = (date) => {
-    return new Date(date).toLocaleDateString('en-US', { 
-        weekday: 'short',
-        month: 'long', 
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-         })
-}
